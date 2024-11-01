3b1ff5eda53f173b81b16e55aaf0ed5c640b16c0
Asif Mustafa <asifmustafa.2o5@gmail.com>
2024-11-02 04:50:04 +0600

    Hero made responsive

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -2,8 +2,12 @@ import Navbar from "./Navbar";
 
 const TopBar = () => {
   return (
-    <div className="flex flex-row justify-between px-14 pt-10">
-      <img className="w-32" src="/logo_one.png" alt="Logo" />
+    <div className="flex flex-row justify-between px-4 md:px-14 md:pt-10 pt-5">
+      <img
+        className="w-24 h-auto object-contain md:w-32"
+        src="/logo_one.png"
+        alt="Logo"
+      />
       <Navbar />
     </div>
   );
@@ -11,26 +15,27 @@ const TopBar = () => {
 
 const Hero = () => {
   return (
-    <div className="min-h-screen flex flex-col space-y-[60vh] m-2 rounded-3xl inter text-white tracking-tight bg-cover bg-[url('/image_one.png')]">
+    <div className="min-h-screen relative inter flex flex-col space-y-10 md:space-y-[60vh] m-2 md:rounded-3xl rounded-xl text-white tracking-tight bg-cover bg-[url('/image_one.png')]">
       <TopBar />
 
-      <div className="w-full pb-28 flex flex-row">
-        <h1 className="w-1/2 px-14 text-6xl font-bold leading-tight">
+      <div className="w-full md:pb-28 md:pt-0 pt-14 flex md:flex-row flex-col">
+        <h1 className="w-full md:w-1/2 px-4 md:px-14 md:pt-10 text-3xl md:text-6xl font-bold leading-tight">
           Build Strategic
           <br />
           Alliances in Marine &<br />
           Offshore Markets.
         </h1>
-        <div className="w-1/2 px-20 flex flex-col items-start justify-center">
+        <div className="md:static absolute bottom-10 w-full md:w-1/2 px-4 pt-20 flex flex-col items-start justify-center">
           <p className="mb-5 text-[#DCDCDC]">
             Specializing in indenting, engineering, technical design,
-            <br /> project management, and consultancy services for the global
-            <br />
+            <br className="hidden md:block" />
+            project management, and consultancy services for the global
+            <br className="hidden md:block" />
             marine industry
           </p>
-          <span className="flex items-center justify-center p-[2px] rounded-md bg-[#4BA5EF] cursor-pointer">
+          <span className="flex items-center justify-center p-2 rounded-md bg-[#4BA5EF] cursor-pointer">
             <span className="mx-5">Learn More</span>
-            <img className="w-10" src="/arrow.png" />
+            <img className="w-6 md:w-10" src="/arrow.png" alt="Arrow" />
           </span>
         </div>
       </div>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,16 +1,83 @@
-const Navbar = () => {
+import React, { useState } from "react";
+import { Link } from "react-scroll";
+
+const MenuButton = ({ onClick }) => {
   return (
-    <nav className="flex items-center justify-center gap-6 font-regular">
-      <span>About Us</span>
-      <span>Services</span>
-      <span>Projects</span>
-      <span>Careers</span>
-      <span>Contact Us</span>
-      <span className="hidden md:flex items-center justify-center p-[2px] rounded-md bg-[#4BA5EF] cursor-pointer">
-        <span className="mx-5">+880 186767 273</span>
-        <img className="w-10" src="/phone.png" />
-      </span>
-    </nav>
+    <button
+      type="button"
+      className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-white rounded-lg md:hidden"
+      onClick={onClick}
+    >
+      <span className="sr-only">Open main menu</span>
+      <svg
+        className="w-5 h-5"
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 17 14"
+      >
+        <path
+          stroke="currentColor"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M1 1h15M1 7h15M1 13h15"
+        />
+      </svg>
+    </button>
+  );
+};
+
+const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const links = [
+    { text: "About Us", id: "about" },
+    { text: "Services", id: "services" },
+    { text: "Projects", id: "projects" },
+    { text: "Careers", id: "careers" },
+    { text: "Contact Us", id: "contact" },
+  ];
+
+  return (
+    <>
+      <nav className="md:flex hidden items-center justify-center gap-6 font-regular">
+        {links.map((link, i) => (
+          <span key={i}>{link.text}</span>
+        ))}
+        <span className="flex items-center justify-center p-[2px] rounded-md bg-[#4BA5EF] cursor-pointer">
+          <span className="mx-5">+880 186767 273</span>
+          <img className="w-10" src="/phone.png" />
+        </span>
+      </nav>
+
+      <nav className="relative md:hidden flex">
+        <MenuButton onClick={() => setIsMenuOpen(!isMenuOpen)} />
+        {isMenuOpen && (
+          <div className="lg:hidden absolute right-2 top-10 z-20 text-black bg-white shadow-md rounded-lg px-5 py-2 flex flex-col items-center border-[0.5px] border-gray-400">
+            {links.map((link, index) => (
+              <React.Fragment key={index}>
+                <Link
+                  className="py-2 hover:text-orange cursor-pointer text-center w-full"
+                  spy={true}
+                  offset={-120}
+                  smooth={true}
+                  to={link.id}
+                  onClick={() => setIsMenuOpen(!isMenuOpen)}
+                >
+                  {link.text}
+                </Link>
+                <hr className="border-t border-gray-300 w-full" />
+              </React.Fragment>
+            ))}
+            <span className="w-[208px] flex items-center my-2 p-[2px] rounded-md text-white bg-[#4BA5EF] cursor-pointer">
+              <span className="mx-5">+880 186767 273</span>
+              <img className="w-10" src="/phone.png" />
+            </span>
+          </div>
+        )}
+      </nav>
+    </>
   );
 };
 
